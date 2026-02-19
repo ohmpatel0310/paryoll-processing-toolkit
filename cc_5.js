43b60ece2ecd2dbde2d549f93e6720b1a7ade9bb
Ohm Patel <ohmp3@usf.edu>
2026-02-19 16:15:06 -0500

    Created Array of Employee Objects

---
+++ cc_5.js
@@ -0,0 +1,7 @@
+// Step 2: Creating an array of employees
+
+let employees = [
+    {name: "Timtom", hourlyRate: 20, hoursWorked: 40},
+    {name: "Bimbom", hourlyRate: 25, hoursWorked: 35},
+    {name: "Jimjom", hourlyRate: 30, hoursWorked: 45}
+]
